f690704c1713ca1259517f1e76db16fd78e4e22e
Luca Pagliaro <luca.pagliaro@thewavestudio.it>
2023-03-17 09:18:07 +0100

    fix: managed errrors with versioning

--- typescript/basic.ts
+++ typescript/basic.ts
@@ -1,3 +1,25 @@
-const a: string = 'ciao';
+type Greeting = string | number;
+type Skill = string | number;
+type Coder = { name: string; skills: Array<Skill> };
 
-console.log({a});
+// Record / Function
+
+// const message = prompt('Ciao');
+// console.log(message.toUpperCase())
+
+let saluto: Greeting = 'ciao';
+saluto = 'hola';
+saluto = 'salve';
+saluto = 'hello';
+saluto = 3;
+
+const coder: Coder = {
+    name: 'Giovanni Giorgio',
+    skills: []
+};
+
+coder.skills.push('JS');
+coder.skills.push(2);
+// coder.skills.push({name: "prova"});
+
+console.log({coder});
