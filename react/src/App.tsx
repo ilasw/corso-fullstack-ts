6d51c5c823f0ba01402a4be68cd3f3c1d5e33dcf
Luca Pagliaro <luca.pagliaro@thewavestudio.it>
2023-03-20 16:02:32 +0100

    feat(react): added app logic:

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('');
+  const inputHandler: React.FormEventHandler<HTMLInputElement> = (event) => {
+    const value = (event.target as HTMLInputElement).value
+    console.log('ciao')
+    setName(value);
+  }
+
+  const names = ['Luigi', 'Giacomo', 'Gianmarco', 'Roberto'];
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {
+        (name?.length > 0)
+          ? <h1>Hi {name}!</h1>
+          : null
+      }
+
+      <div style={{ marginTop: '40px' }}>
+        <input type="text"
+          value={name}
+          onInput={inputHandler} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <ul>
+        { names.map((name) => (
+          <li>{name}</li>
+        )) }
+      </ul>
     </div>
   )
 }
