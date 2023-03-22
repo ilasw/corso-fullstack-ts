e150c611340bb0a157b99d7bd31079d97caf55c6
Luca Pagliaro <luca.pagliaro@thewavestudio.it>
2023-03-22 17:17:53 +0100

    feat(rxjs): added project and first examples

---
+++ rxjs/src/main.ts
@@ -0,0 +1,46 @@
+import { of, map, fromEvent, tap, from, interval, take} from 'rxjs';
+
+// of('Hello World')
+//   .pipe(
+//     map((stringa) => stringa.toUpperCase()),
+//     map((text) => text.concat('!'))
+//   ).subscribe();
+
+// const formEls = document.querySelectorAll('form');
+// const clickListener$ = fromEvent(formEls, 'click').pipe(
+//   tap(event => console.log({ event })), // esegue, ma il return del tap viene ignorato nella catena
+//   map(event => event.target), // il map modifica il valore e il suo return sarà l'input per la funzione successiva
+//   tap(target => console.log({ target })),
+// );
+
+// const subscription = clickListener$.subscribe();
+// from(['a', 'b', 'c']).pipe(
+//   tap((value) => console.log({ value }))
+// ).subscribe()
+
+
+// Scrivere codice che ogni secondo aggiorna 
+// la pagina inserendo a schermo l'ora esatta.
+// interval(1_000).pipe(
+//   tap((value) => console.log(value)),
+//   map(() => new Date()),
+//   map((date) => {
+//     const withTwoChars = (number:number) => String(number).padStart(2, '0');
+
+//     return `${date.getHours()} : ${withTwoChars(date.getMinutes())} : ${withTwoChars(date.getSeconds())}`;
+//   }),
+//   take(10),
+// ).subscribe((dataPortataAStringa) => {
+//   // dentro il subscrive devo scrivere le operazioni che vanno fatte ad ogni emit
+//   // -> stampare la data;
+//   document.body.innerHTML = dataPortataAStringa;
+// })
+
+// Bingo with RxJS
+// esegui un console.log di 6 numeri randomici; ??
+// Bonus: annuncia i risultati distanziati 1s l'uno dall'altro; // "interval"
+const allNumbers = Array.from({length: 90}, (_, i) => i+1);
+
+(Observable()).subscribe((number) => console.log(number) )
+
+export { }
