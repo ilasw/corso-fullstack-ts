e36e323955a6a496cb78a6acb1449a0026380e1f
Luca Pagliaro <pagliaroluca@gmail.com>
2023-03-23 15:53:53 +0100

    feat(angular): added api call

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  private httpClient = inject(HttpClient)
+
+  // cea52831eed64f90961345afeec19f5e
   count = 0;
+  items: string[] = ['Pippo', 'pluto', 'paperino'];
+
+  url = "https://newsapi.org/v2/everything?q=Italia&sortBy=publishedAt&apiKey=cea52831eed64f90961345afeec19f5e&page=2&pageSize=10"
 
   increment(){
     this.count += 1
+    console.log(this.httpClient);
+
+    this.httpClient.get(this.url)
+    .subscribe((value) => console.log({value}))
   }
 
   decrement(){
