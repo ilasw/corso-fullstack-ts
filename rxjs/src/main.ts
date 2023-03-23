6d3f78041546ca8f8b32b8c1b1dfcae07b527311
Luca Pagliaro <luca.pagliaro@thewavestudio.it>
2023-03-23 13:10:25 +0100

    feat(rxjs): added solution for autocomplete

--- rxjs/src/main.ts
+++ rxjs/src/main.ts
@@ -1,5 +1,5 @@
-import { of, map, fromEvent, tap, from, interval, take, range, startWith, scan, filter, debounceTime, pairwise, timer, switchMap } from 'rxjs';
-import ajax from 'rxjs/ajax';
+import { of, map, fromEvent, tap, from, interval, take, range, startWith, scan, filter, debounceTime, pairwise, timer, switchMap, concatMap } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 // of('Hello World')
 //   .pipe(
 //     map((stringa) => stringa.toUpperCase()),
@@ -77,16 +77,16 @@ import ajax from 'rxjs/ajax';
   // type EventWithTarget<MioEvento = UIEvent, ElementoTarget = Element> = MioEvento & {target: ElementoTarget};
   // fromEvent<EventWithTarget<InputEvent, HTMLInputElement>>(inputEl, 'input')
 
-  fromEvent<UIEvent>(inputEl, 'input')
-    .pipe(
-      filter(isEventAndInputEventWithTarget),
-      debounceTime(250),
-      map((event) => event.target.value.trim()),
-      filter(text => text.length >= 3),
-    )
-    .subscribe({
-      next: (value) => console.log({ value })
-    });
+  // fromEvent<UIEvent>(inputEl, 'input')
+  //   .pipe(
+  //     filter(isEventAndInputEventWithTarget),
+  //     debounceTime(250),
+  //     map((event) => event.target.value.trim()),
+  //     filter(text => text.length >= 3),
+  //   )
+  //   .subscribe({
+  //     next: (value) => console.log({ value })
+  //   });
 
   // Type guards
   function isEventAndInputEventWithTarget(event: UIEvent): event is (InputEvent & { target: HTMLInputElement }) {
@@ -112,29 +112,61 @@ import ajax from 'rxjs/ajax';
   console.info(`switchMap(x => of(x, x ** 2, x ** 3))`);
 
 
-  const switched = of(1, 2, 3)
+  // const switched = of(1, 2, 3)
+  //   .pipe(
+  //     tap((value) => console.log(`valore che deriva da of()`, value)), // 1
+  //     switchMap(x => of(x, x ** 2, x ** 3)), // invia 3 valori partendo da 1
+  //     tap((value) => console.log(`valore che deriva dallo switchMap()`, value))
+  //   )
+  //   .subscribe(x => console.log(`Valore che arriva al subcriber`, x));
+
+  /*
+    
+    Partendo dall'esercizio sull'input fatto in precedenza:
+    al posto di fare il console.log, dopo il filter, 
+    eseguire una chiamata API usando ajax.getJSON
+
+    
+
+    stampare i risultati a schermo;
+
+    import {ajax} from 'rxjs/ajax';
+
+    ajax.getJSON("URL") -> Osservabile
+  
+  */
+
+  const inputEl = document.querySelector('input.autocomplete');
+  const resultsEl = document.querySelector('ul.results');
+
+  if (!inputEl || !resultsEl) return;
+
+  const baseURL = `https://chroniclingamerica.loc.gov/suggest/titles/?q=`;
+  type ArticleResponse = [string, string[], string[], string[]];
+
+  fromEvent<UIEvent>(inputEl, 'input')
     .pipe(
-      tap((value) => console.log(`valore che deriva da of()`, value)), // 1
-      switchMap(x => of(x, x ** 2, x ** 3)), // invia 3 valori partendo da 1
-      tap((value) => console.log(`valore che deriva dallo switchMap()`, value))
+      debounceTime(250),
+      filter(isEventAndInputEventWithTarget),
+      tap(() => resultsEl.innerHTML = ``),
+      map((event) => event.target.value),
+      switchMap(text => ajax.getJSON<ArticleResponse>(baseURL.concat(text))),
+      tap(response => console.log(`API response:`, response)),
+      map((response) => response[1]),
+      concatMap((titles) => of(...titles)), // emetto per ogni elemento dell'array
+      tap(value => console.log(value))
     )
-    .subscribe(x => console.log(`Valore che arriva al subcriber`, x));
+    .subscribe({
+      next: (title) => {
+        resultsEl.innerHTML += `<li>${title}</li>`;
+      }
+    })
 
-    /*
-      
-      Partendo dall'esercizio sull'input fatto in precedenza:
-      al posto di fare il console.log, dopo il filter, 
-      eseguire una chiamata API usando ajax.getJSON
 
-      https://chroniclingamerica.loc.gov/suggest/titles/?q=Florida
+  function isEventAndInputEventWithTarget(event: UIEvent): event is (InputEvent & { target: HTMLInputElement }) {
+    return event instanceof InputEvent && !!(event.target) && event.target instanceof HTMLInputElement;
+  }
 
-      stampare i risultati a schermo;
-
-      ajax.getJSON("URL") -> Osservabile
-    
-    */
-
-    
 })()
 
 
