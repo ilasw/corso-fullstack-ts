5904ae33cb622a41f4c1d7c85690c3fccd411b9c
Luca Pagliaro <pagliaroluca@gmail.com>
2023-03-28 09:33:43 +0200

    feat(angular): added empty modal component

--- angular/src/app/components/modal/modal.component.ts
+++ angular/src/app/components/modal/modal.component.ts
@@ -11,7 +11,22 @@ export class ModalComponent {
 
   /*
 
+  Scrivere un componente modale.
+  Una modale non è altro che un div in posizione fixed che si
+  pone sopra tutti gli altri elementi della pagina.
+  Viene caratterizzata da due elementi:
+   - Una shadow, che è uno sfondo generalmente scuro;
+   - Un modal-body che è il nostro div con sfondo bianco che contiente HTML
 
+  Al click sulla shadow dobbiamo far chiudere la modale.
+
+  Bonus: Mettiamo dentro la modale un bottone "Accetta" che al click fa
+  chiudere la modale, per differenziarlo dalla chiusura "shadow" emettiamo
+  un valore differente. Il componente padre deve fare un console.log particolare
+  quando questo avviene.
+
+  Il componente deve mostrare il suo contenuto solo se @Input isOpen è true;
+  Emettere un evento al click su shadow (e eventuale button accetta).
 
   */
 }
