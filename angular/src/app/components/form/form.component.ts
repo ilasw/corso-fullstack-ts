8b844c72a09563106de05657790c8ceec8c3d34a
Luca Pagliaro <pagliaroluca@gmail.com>
2023-03-30 17:02:02 +0200

    feat(angular): added exercise with form and services

--- angular/src/app/components/form/form.component.ts
+++ angular/src/app/components/form/form.component.ts
@@ -1,6 +1,6 @@
 import { map } from 'rxjs';
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -8,19 +8,37 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
-  model = {
-    name: new FormControl<string>('', [Validators.minLength(20), Validators.maxLength(500), Validators.required]),
-    surname: '',
-    address: ''
-  }
 
-  nameToUpperCase$ = this.model.name.valueChanges.pipe(
-    map(text => text?.toUpperCase())
+  // Creare form con email e pw,
+  // al submit stampare i dati in un console.log
+
+  // portiamo il form a FormGroup e creiamo una pagina
+  // per la registrazione di un user
+
+  // al submit del form richiamare il metodo
+  // del servic AuthService.register()
+
+  // AuthService è un service che creeremo oggi, si occupa di creare/cercare/loggare user.
+  // Ha 3 metodi: search(username), register({username, password}), login({ username, password })
+
+  // - creare un utente significa salvarlo dentro il localStorage
+  // es: localStorage.set('user-[USER.USERNAME]', JSON.stringify(user))
+  // - Recuperare un utente by username significa cercare la chiave user-{USERNAME}
+  // es: localStorage.get('user-ilasw') -> null | string
+  // - Loggare utente significa cercare lo user relativo e fare check sulla sua pw
+
+  model = new FormGroup({
+    firstName: new FormControl<string>('', [Validators.minLength(20), Validators.maxLength(500), Validators.required]),
+    lastName: new FormControl<string>('', [Validators.minLength(20), Validators.maxLength(500), Validators.required])
+  })
+
+  nameUppercase$ = this.model.controls.firstName.valueChanges.pipe(
+
   )
 
-  submit($event: SubmitEvent){
-    $event.preventDefault();
-    const name = this.model.name.value;
-    console.log({name})
+  submit(){
+    const {firstName, lastName} = this.model.value;
+    console.log({firstName, lastName})
+    this.model.getError('firstName');
   }
 }
